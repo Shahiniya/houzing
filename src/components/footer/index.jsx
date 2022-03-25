272a70e4495081e2f2d3935545b616f862057630
Shelli <sh.panjieva@gmail.com>
2022-03-25 23:04:12 +0900

    new

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BigWrapper, Contacts, ContactUs,Img, Links, MiddleWrapper } from './style'
+import { Logo,Logos,BigWrapper, Contacts, ContactUs,Discover,Img, Links, LinksWrapper, ListCategory, MiddleWrapper, QuickLinks, Subscribe, LastWrapper, LastWrapper1, LastWrapper2 } from './style'
 import {footer, footer1} from '../../utils/footer'
 
 export const Footer = () => {
   return (
     <BigWrapper>
         <MiddleWrapper>
+        <Logo/>
            <ContactUs>
             <h4>Contact Us</h4>
                 {
@@ -18,6 +19,7 @@ export const Footer = () => {
                         )
                     })
                 }
+                <LinksWrapper>
                 {
                     footer1.map((value)=>{
                         return(
@@ -27,8 +29,60 @@ export const Footer = () => {
                         )
                     })
                 }
+                </LinksWrapper>
            </ContactUs>
+          
+           <Discover>
+                <h3>Discover</h3>
+                <h4>Chicago</h4>
+                <h4>Los Angeles</h4>
+                <h4>Miami</h4>
+                <h4>NewYork</h4>
+           </Discover>
+
+           <ListCategory>
+                <h3>List by Category</h3>
+                <h4>Apartments</h4>
+                <h4>Condos</h4>
+                <h4>Houses</h4>
+                <h4>Offices</h4>
+                <h4>Retail</h4>
+                <h4>Villas</h4>
+            </ListCategory>
+
+            <QuickLinks>
+                <h3>Quick Links</h3>
+                <h4>About us</h4>
+                <h4>Term & Conditions</h4>
+                <h4>User's Guide</h4>
+                <h4>Support Center</h4>
+                <h4>Press Info</h4>
+                <h4>Contact Us</h4>
+            </QuickLinks>
+
+            <Subscribe>
+                <h3>Subscribe</h3>
+                <h4>We don't send spassm don't worry</h4>
+                <input type='text' placeholder='Enter your email' />
+           </Subscribe>
         </MiddleWrapper>
+
+        <LastWrapper>
+            <h6>Copyright 2021 CreativeLayers.All Right Reserved</h6>
+             <LastWrapper1>
+            <Logos/>
+            <h4>Houzing</h4>
+            </LastWrapper1>
+
+            <LastWrapper2>
+                <h5>Home</h5>
+                <h5>Site Map</h5>
+                <h5>Privacy Policy</h5>
+                <h5>Cookie Policy</h5>
+            </LastWrapper2>
+         </LastWrapper>
     </BigWrapper>
+
+    
   )
 }
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,60 +1,249 @@
 import styled from 'styled-components';
-
+import { ReactComponent as telg } from '../../assets/icons/twitter.svg';
+import { ReactComponent as logo } from '../../assets/icons/logo.svg';
 const BigWrapper = styled.div`
 width:100%;
-height:600px;
+height:650px;
 background-color:#0D263B;
+margin:20px 20px 60px 0px;
+position:relative;
+
 `;
 
 const MiddleWrapper = styled.div`
-margin:40px 20px 60px 0;
-background-color:green;
+display:flex;
+margin:70px 20px 60px 150px;
+/* background-color:green; */
+width:1700px;
+height:500px;
 
 `;
 
 const ContactUs = styled.div`
-width:400px;
-height:500px;
-background-color:red;
+width:250px;
+height:300px;
+/* background-color:red; */
+margin:80px 20px 0px 20px;
+display:flex;
+flex-direction:column;
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color:#ffffff;
+margin:10px;
+}
 `;
 const Contacts = styled.div`
-background-color:green;
-width:300px;
-height:80px;
+/* background-color:green; */
+width:250px;
+height:30px;
+display:flex;
+margin:20px 0 15px 6px;
+h6{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin-left:10px;
 
+}
+`;
+const LinksWrapper = styled.div`
+display:flex;
+width:150px;
+height:30px;
 `;
 const Links = styled.div`
-background-color:green;
-width:200px;
-height:40px;
-display:flex;
+/* flex-direction:row;s */
+/* background-color:green; */
+width:100px;
+height:30px;
+
 `;
 
 const Img = styled.img`
-width:20px;
-height:20px;
+width:15px;
+height:15px;
+path{
+    fill:#ffff;
+}
 /* background:white; */
 `;
-
+const Logo = styled(logo)`
+width:423px;
+height:520px;
+position:absolute;
+left:80px;
+top:20px;
+opacity:0.2;
+:hover{
+    color:rgba(255, 255, 255, 1);
+}
+`;
 const Discover = styled.div`
-
+width:250px;
+height:300px;
+/* background-color:red; */
+margin:80px 20px 0px 100px;
+h3{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color: #FFFFFF;
+margin:13px
+}
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin:30px;
+}
 `;
 const ListCategory = styled.div`
-
+width:250px;
+height:300px;
+/* background-color:red; */
+margin:80px 20px 0px 20px;
+h3{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color: #FFFFFF;
+margin:13px
+}
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin:30px;
+}
 `;
 const QuickLinks = styled.div`
-
+width:250px;
+height:300px;
+/* background-color:red; */
+margin:80px 20px 0px 20px;
+h3{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color: #FFFFFF;
+margin:13px
+}
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin:30px;
+}
 `;
 const Subscribe = styled.div`
-
+width:250px;
+height:300px;
+/* background-color:red; */
+margin:80px 20px 0px 20px;
+h3{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+color: #FFFFFF;
+margin:13px
+}
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin:30px;
+}
+input{
+    width:469px;
+    height:60px;
+    border-radius:3px;
+    margin:20px;
+}
 `;
 
 const LastWrapper = styled.div`
+width:100%;
+height:150px;
+display:flex;
+justify-content:space-around;
+border-top:2px solid rgba(255, 255, 255, 0.15);;
+/* opacity: 0.2;; */
+h6{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+width:347;
+height:18px;
+margin:40px 60px;
+opacity:0.7;
 
+
+}
+`;
+const LastWrapper1 = styled.div`
+width:300px;
+height:100px;
+display:flex;
+margin:10px 50px;
+h4{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+color: #FFFFFF;
+margin:20px 0px;
+
+}
+`;
+const Logos = styled(logo)`
+width:40px;
+height:48px;
+margin:10px;
+margin-left:60px;
+
+path{
+    fill:white
+}
+
+`;
+
+const LastWrapper2 = styled.div`
+width:400px;
+height:100px;
+display:flex;
+margin:40px 30px;
+h5{
+font-family: 'Cerebri Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+color: #FFFFFF;
+margin:0px 10px;
+opacity:0.7;
+}
 `;
 
 
 
-export {Links,Img,Contacts,BigWrapper,MiddleWrapper,
-    ContactUs,Discover,ListCategory,
+export {Logos,LastWrapper1,LastWrapper2,LinksWrapper,Links,Img,Contacts,BigWrapper,MiddleWrapper,
+    ContactUs,Logo,Discover,ListCategory,
     QuickLinks,Subscribe,LastWrapper};
